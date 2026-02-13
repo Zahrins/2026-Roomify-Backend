e3bb2f7291776454313f54e1393abc4d558819e3
Zahrins <khadijahzahrinsavana@gmail.com>
2026-02-13 22:45:14 +0700

    feat: add BookingStatusHistory migration to track status changes

---
+++ 2026-Roomify-Backend/Migrations/20260213115738_ChangeTanggalToDate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2026_Roomify_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTanggalToDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "role",
+                value: "user");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "role",
+                value: "User");
+        }
+    }
+}
